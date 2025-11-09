1e541d1c082e709abc18cd70125a6aa36939399b
YongHoon Lee <yonghoonleework@gmail.com>
2025-11-09 19:16:51 +0100

    9th Nov Commit and Push
    Added Partial Transposition terms

--- ham_search.py
+++ ham_search.py
@@ -5,14 +5,14 @@ epsilon = 0.01
 
 v_x = 0
 v_y = 0
-v_z = 0.3
+v_z = 0.9
 
 m_x = 0
 m_y = 0
-m_z = 0.3
+m_z = 0.9
 
-k_min, k_max, k_points = 0, 30, 200
-s_min, s_max, s_points = 0, 30, 200
+k_min, k_max, k_points = 0, 50, 100
+s_min, s_max, s_points = 0, 50, 100
 
 lambda0_v = 2*(v_x**2 + v_y**2)/(1 + v_x**2 + v_y**2 + v_z**2)
 lambda0_m = 2*(m_x**2 + m_y**2)/(1 + m_x**2 + m_y**2 + m_z**2)
@@ -71,53 +71,77 @@ def build_operator(M_ij):
     H = np.zeros((4, 4), dtype=complex)
     for i in range(4):
         for j in range(4):
-            H += M_ij[i, j]  * SIGMA_TENSORS[i][j]
+            H += M_ij[i, j]  * SIGMA_TENSORS[j][i]
 
     return H
 
-def lind_noise(k):
+def notFullKraus_lind_noise(k):
 
     A = np.array( [
-        [0, v[1]*m[0], v[2]*m[0], v[3]*m[0]],
+        [0, v[1]*m[0], v[2]*m[0], 2 * v[3]*m[0]],
         [v[0]*m[1], 2*v[1]*m[1], 2*v[2]*m[1], 3*v[3]*m[1]],
         [v[0]*m[2], 2*v[1]*m[2], 2*v[2]*m[2], 3*v[3]*m[2]],
         [2*v[0]*m[3], 3*v[1]*m[3], 3*v[2]*m[3], 4*v[3]*m[3]]
     ])
 
-    A = A * -0.5 * k
+    return -0.5 * k * build_operator(A)
 
-    return build_operator(A)
+def notFullKraus_lind_noise_PT(k):
+
+    A = np.array([
+        [0, v[1] * m[0], -v[2] * m[0], v[3] * m[0]],
+        [v[0] * m[1], 2 * v[1] * m[1], -2 * v[2] * m[1], 3 * v[3] * m[1]],
+        [v[0] * m[2], 2 * v[1] * m[2], -2 * v[2] * m[2], 3 * v[3] * m[2]],
+        [2 * v[0] * m[3], 3 * v[1] * m[3], -3 * v[2] * m[3], 4 * v[3] * m[3]]
+
+    ])
+
+    return -0.5 * k * build_operator(A)
 
 
 def lind_hamiltonian_zz(s):
 
     H = np.array([
-        [0 , -v[2]*m[3] , v[1]*m[3], 0],
-        [-v[3]*m[2] , 0 , -1j*v[1]*(m[1]+m[2]) , -m[2]],
-        [v[3]*m[1], -1j*v[2]*(m[1]+m[2]) , 0 , m[1]],
-        [0 , -v[2], v[1], 0]
+        [0 , 0, 0, 0],
+        [0, 0, 0, -m[2]],
+        [0,0,0, m[1]],
+        [0, -v[2], v[1], 0]
 
     ])
 
-    H = 0.5 * s * H
+    return 0.5 * s * build_operator(H)
 
-    return build_operator(H)
+def lind_hamiltonian_zz_PT(s):
 
+    H = np.array([
+        [0 , -v[2]*m[3] , -v[1]*m[3], 0],
+        [-m[2]*v[3] , 0, 0, -m[2]],
+        [m[1]*v[3], 0, 0, m[1]],
+        [0 , -v[2], -v[1], 0]
 
-def lindblad(k,s):
-    return lind_noise(k) + lind_hamiltonian_zz(s)
+    ])
+    return 0.5 * s * build_operator(H)
 
-def final_dm(k,s):
+def notFullKraus_dm(k,s):
     A = np.array([
         [1,v[1],v[2],v[3]],
         [m[1],v[1]*m[1],v[2]*m[1],v[3]*m[1]],
         [m[2],v[1]*m[2],v[2]*m[2],v[3]*m[2]],
         [m[3], v[1]*m[3], v[2]*m[3],v[3]*m[3]]
     ])
-    A = 0.25 * A
 
-    return build_operator(A) + epsilon * lindblad(k,s)
+    return 0.25 * build_operator(A) + epsilon * (notFullKraus_lind_noise(k) + lind_hamiltonian_zz(s))
 
+
+def notFullKraus_dm_PT(k,s):
+    A = np.array([
+        [1, v[1], -v[2], v[3]],
+        [m[1], v[1] * m[1], -v[2] * m[1], v[3] * m[1]],
+        [m[2], v[1] * m[2], -v[2] * m[2], v[3] * m[2]],
+        [m[3], v[1] * m[3], -v[2] * m[3], v[3] * m[3]]
+    ])
+
+    return 0.25 * build_operator(A) + epsilon * (notFullKraus_lind_noise_PT(k) + lind_hamiltonian_zz_PT(s))
 # =========================
 # 4) Build operator and compute lowest eigenvalue
 # =========================
@@ -125,8 +149,9 @@ def final_dm(k,s):
 
 def lowest_eigenvalue(M_func, k, s):
     M = M_func(k, s)
-    H = build_operator(M)
-    vals = np.linalg.eigvalsh(H)
+    #H = build_operator(M)
+    vals = np.linalg.eigvalsh(M)
+    #print(np.min(vals).real)
     return float(np.min(vals).real)
 
 # =========================
@@ -145,13 +170,13 @@ def compute_sign_grid(M_func, k_grid, s_grid):
 # =========================
 if __name__ == "__main__":
 
-
-
+    #print(build_operator(np.array([[0,1,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]])))
 
     k_grid = np.linspace(k_min, k_max, k_points)
     s_grid = np.linspace(s_min, s_max, s_points)
 
-    Z_H = compute_sign_grid(final_dm, k_grid, s_grid)
+    Z_notFull = compute_sign_grid(notFullKraus_dm, k_grid, s_grid)
+    Z_notFull_PT = compute_sign_grid(notFullKraus_dm_PT, k_grid, s_grid)
 
 
     # =========================
@@ -161,28 +186,28 @@ if __name__ == "__main__":
 
     cmap = plt.get_cmap('gray', 2)  # blue for negative, red for nonnegative
 
-    im1 = axes[0].imshow(Z_H, extent=[s_min, s_max, k_min, k_max],
+    im1 = axes[0].imshow(Z_notFull, extent=[s_min, s_max, k_min, k_max],
                          origin='lower', cmap=cmap, vmin=0, vmax=1, aspect='auto')
     axes[0].plot([s_min, s_max], [s_min, s_max], 'r', linewidth=1, label = "k=s")
 
     axes[0].set_xlim(s_min, s_max)
     axes[0].set_ylim(k_min, k_max)
-    axes[0].set_title("Not Full Kraus Rank: negative vs non-negative regions")
+    axes[0].set_title("Density Matrix : Not Full Kraus Rank")
     axes[0].set_xlabel("s")
     axes[0].set_ylabel("k")
     axes[0].legend()
 
-    # im2 = axes[1].imshow(Z_Q, extent=[s_min, s_max, k_min, k_max],
-    #                      origin='lower', cmap=cmap, vmin=0, vmax=1, aspect='auto')
-    # axes[1].plot([s_min, s_max], [s_min, s_max], 'r', linewidth=1,label = "k=s")
-    # if lambda0 > 10e-8:
-    #     axes[1].plot([s_min, s_max], [(1/lambda0)*s_min, (1/lambda0)*s_max], 'b', linewidth=1, label = "lambda condition")
-    # axes[1].set_xlim(s_min, s_max)
-    # axes[1].set_ylim(k_min, k_max)
-    # axes[1].set_title("Full Kraus Rank: negative vs non-negative regions")
-    # axes[1].set_xlabel("s")
-    # axes[1].set_ylabel("k")
-    # axes[1].legend()
+    im2 = axes[1].imshow(Z_notFull_PT, extent=[s_min, s_max, k_min, k_max],
+                         origin='lower', cmap=cmap, vmin=0, vmax=1, aspect='auto')
+    axes[1].plot([s_min, s_max], [s_min, s_max], 'r', linewidth=1,label = "k=s")
+    if lambda0 > 10e-8:
+        axes[1].plot([s_min, s_max], [(1/lambda0)*s_min, (1/lambda0)*s_max], 'b', linewidth=1, label = "lambda condition")
+    axes[1].set_xlim(s_min, s_max)
+    axes[1].set_ylim(k_min, k_max)
+    axes[1].set_title("Partial Transpose : Not Full Kraus Rank")
+    axes[1].set_xlabel("s")
+    axes[1].set_ylabel("k")
+    axes[1].legend()
 
     cbar = fig.colorbar(im1, ax=axes, ticks=[0, 1], fraction=0.05, pad=0.04)
     cbar.ax.set_yticklabels(['Negative', '≥ 0'])
